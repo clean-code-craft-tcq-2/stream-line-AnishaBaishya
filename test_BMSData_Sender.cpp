7342b4828b5eac1e7f24acf86566c88b1bb8b6a5
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 19:20:33 +0530

    Update test_BMSData_Sender.cpp

--- test_BMSData_Sender.cpp
+++ test_BMSData_Sender.cpp
@@ -39,17 +39,17 @@ TEST_CASE("Test 1 : Prepare Data from Sensor : Valid Data") {
 
 TEST_CASE("Test 2 : Prepare Data from Sensor : Invalid Data") {
 
-  BatteryParameterInfo parameterInfo [NoOfParameter] ;
-  Sender DataFromSender ;
+  BatteryParameterInfo parameterInfo1 [NoOfParameter] ;
+  Sender DataFromSender1 ;
   
-  parameterInfo[0].minimumThreshold = 0.0;
-  parameterInfo[0].maximumThreshold = 60.0;
+  parameterInfo1[0].minimumThreshold = 0.0;
+  parameterInfo1[0].maximumThreshold = 60.0;
   
-  parameterInfo[1].minimumThreshold = 20.0;
-  parameterInfo[1].maximumThreshold = 80.0;
+  parameterInfo1[1].minimumThreshold = 20.0;
+  parameterInfo1[1].maximumThreshold = 80.0;
   
-  DataFromSender.number_of_values = 50;
+  DataFromSender1.number_of_values = 50;
   float (*funp_ReadDataFromSensor)(float,float) = SimulateReadDataFromSensorInvalid;
   
-  REQUIRE(SenderData(parameterInfo ,&DataFromSender,funp_ReadDataFromSensor) == FALSE);
+  REQUIRE(SenderData(parameterInfo1 ,&DataFromSender1,funp_ReadDataFromSensor) == FALSE);
 }
