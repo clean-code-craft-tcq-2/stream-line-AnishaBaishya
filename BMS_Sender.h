dbddf35da5b595ffa4a4221642bd74b3017b6d03
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 18:47:22 +0530

    Update BMS_Sender.h

--- BMS_Sender.h
+++ BMS_Sender.h
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define TRUE  0
+#define FALSE 1
+
 typedef enum {
   TempParameter,
   SOCParameter,
@@ -20,9 +23,6 @@ typedef struct  {
   int number_of_values;
   char formatted_senderdata[50];
 } Sender;
-
-#define TRUE  0
-#define FALSE 1
 
 
 bool SenderData(BatteryParameterInfo *parameterInfo ,Sender *DataFromSender, float (*funp_ReadDataFromSensor)(float,float));
