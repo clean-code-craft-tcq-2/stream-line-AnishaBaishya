6b767a79e5ff2d37c4438ee1abfac3d5ffb9d86c
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 13:58:45 +0530

    Update test_BMSData_Sender.cpp

--- test_BMSData_Sender.cpp
+++ test_BMSData_Sender.cpp
@@ -7,6 +7,14 @@
 TEST_CASE("Test 1 : Prepare Data from Sensor") {
 
   BatteryParameterInfo parameterInfo [NoOfParameter] ;
+  parameterInfo[0].minimumThreshold = 0;
+  parameterInfo[0].maximumThreshold = 45.0;
+    
+  parameterInfo[1].minimumThreshold = 20;
+  parameterInfo[1].maximumThreshold = 80;
+
+  parameterInfo[2].minimumThreshold = 0;
+  parameterInfo[2].maximumThreshold = 0.8;
   Sender DataFromSender ;
   DataFromSender.number_of_values = 5;
   REQUIRE(SenderData(parameterInfo ,DataFromSender,funp_ReadDataFromSensor) == 1);
