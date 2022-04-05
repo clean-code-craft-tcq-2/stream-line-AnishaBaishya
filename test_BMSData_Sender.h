d060b1e0f79a19354420aa05307f915e96aaf971
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 15:09:47 +0530

    Update test_BMSData_Sender.h

--- test_BMSData_Sender.h
+++ test_BMSData_Sender.h
@@ -1,10 +1,14 @@
 #include <time.h>
 #include <stdlib.h>
 
-float SimulateReadDataFromSensor()
+float SimulateReadDataFromSensor(float minimumThreshold,float maximumThreshold)
 {
   srand(time(0));
-  float data = (rand() % (int)(100)) ;
+  float data = (rand() % (int)(maximumThreshold+1)) ;
+  if(data < minimumThreshold)
+  {
+    data = data + minimumThreshold;
+  }
   return data;
 }
 
