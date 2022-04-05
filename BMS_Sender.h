388d68659a8af8563e46e53754fbe82f28511b7c
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 12:41:35 +0530

    Update BMS_Sender.h

--- BMS_Sender.h
+++ BMS_Sender.h
@@ -1,1 +1,23 @@
 int SenderData();
+
+
+
+typedef enum {
+  TempParameter,
+  SOCParameter,
+  ChargeRateParameter,
+  NoOfParameter
+} EV_BatteryParameterTypesForBMS;
+
+
+typedef struct {
+  EV_BatteryParameterTypesForBMS parameter;
+  float minimumThreshold;
+  float maximumThreshold;
+  char parameterName[100];
+} BatteryParameterInfo;
+
+typedef struct  {
+  int number_of_values;
+  string formatted_senderdata[50];
+} Sender;
