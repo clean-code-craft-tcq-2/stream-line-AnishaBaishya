48cbcf94c5b66c153df760fc5e86b1686825006e
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 13:29:18 +0530

    Update test_BMSData_Sender.cpp

--- test_BMSData_Sender.cpp
+++ test_BMSData_Sender.cpp
@@ -1,11 +1,13 @@
 #define CATCH_CONFIG_MAIN 
 
 #include "test/catch.hpp"
+#include "test_BMSData_Sender.h"
 #include "BMS_Sender.h"
 
 TEST_CASE("Test 1 : Prepare Data from Sensor") {
 
   BatteryParameterInfo parameterInfo [NoOfParameter] ;
   Sender SenderData ;
+  SenderData.number_of_values = 5;
   REQUIRE(SenderData(parameterInfo ,SenderData,funp_ReadDataFromSensor) == 1);
 }
