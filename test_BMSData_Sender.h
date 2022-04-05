300674c101469c3b93e5a6be0adb6c6fd9203120
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 18:43:57 +0530

    Delete test_BMSData_Sender.h

--- test_BMSData_Sender.h
+++
@@ -1,8 +0,0 @@
-float SimulateReadDataFromSensor(float minimumThreshold,float maximumThreshold)
-{
-  float data = (rand() % (int)(maximumThreshold - minimumThreshold +1)) + minimumThreshold ;
-  return data;
-}
-
-
-float (*funp_ReadDataFromSensor)(float,float) = SimulateReadDataFromSensor;
