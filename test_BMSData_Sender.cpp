0f35d175945a2dfa0426a82d634cbfd7111825ca
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 19:19:03 +0530

    Update test_BMSData_Sender.cpp

--- test_BMSData_Sender.cpp
+++ test_BMSData_Sender.cpp
@@ -12,11 +12,13 @@ float SimulateReadDataFromSensor(float minimumThreshold,float maximumThreshold)
   return data;
 }
 
+float SimulateReadDataFromSensorInvalid(float minimumThreshold,float maximumThreshold)
+{
+  float data = NULL ;
+  return data;
+}
 
-float (*funp_ReadDataFromSensor)(float,float) = SimulateReadDataFromSensor;
-
-
-TEST_CASE("Test 1 : Prepare Data from Sensor") {
+TEST_CASE("Test 1 : Prepare Data from Sensor : Valid Data") {
 
   BatteryParameterInfo parameterInfo [NoOfParameter] ;
   Sender DataFromSender ;
@@ -28,6 +30,26 @@ TEST_CASE("Test 1 : Prepare Data from Sensor") {
   parameterInfo[1].maximumThreshold = 80.0;
   
   DataFromSender.number_of_values = 50;
+  float (*funp_ReadDataFromSensor)(float,float) = SimulateReadDataFromSensor;
+  
   srand(time(0));
   REQUIRE(SenderData(parameterInfo ,&DataFromSender,funp_ReadDataFromSensor) == TRUE);
+}
+
+
+TEST_CASE("Test 2 : Prepare Data from Sensor : Invalid Data") {
+
+  BatteryParameterInfo parameterInfo [NoOfParameter] ;
+  Sender DataFromSender ;
+  
+  parameterInfo[0].minimumThreshold = 0.0;
+  parameterInfo[0].maximumThreshold = 60.0;
+  
+  parameterInfo[1].minimumThreshold = 20.0;
+  parameterInfo[1].maximumThreshold = 80.0;
+  
+  DataFromSender.number_of_values = 50;
+  float (*funp_ReadDataFromSensor)(float,float) = SimulateReadDataFromSensorInvalid;
+  
+  REQUIRE(SenderData(parameterInfo ,&DataFromSender,funp_ReadDataFromSensor) == FALSE);
 }
