9ee7a1b6cf23c678d17be5841834aa1c903e9a28
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 18:20:24 +0530

    Update test_BMSData_Sender.cpp

--- test_BMSData_Sender.cpp
+++ test_BMSData_Sender.cpp
@@ -15,6 +15,6 @@ TEST_CASE("Test 1 : Prepare Data from Sensor") {
   parameterInfo[1].minimumThreshold = 20.0;
   parameterInfo[1].maximumThreshold = 80.0;
   
-  DataFromSender.number_of_values = 20;
+  DataFromSender.number_of_values = 40;
   REQUIRE(SenderData(parameterInfo ,&DataFromSender,funp_ReadDataFromSensor) == TRUE);
 }
