8dbd75c5d1546c7cb6ff63a18ea29972faa7d4c1
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-05 15:12:23 +0530

    Update BMS_Sender.c

--- BMS_Sender.c
+++ BMS_Sender.c
@@ -1,14 +1,14 @@
 #include "BMS_Sender.h"
 
 
-int SenderData(BatteryParameterInfo *parameterInfo ,Sender DataFromSender, float (*funp_ReadDataFromSensor)()) {
+int SenderData(BatteryParameterInfo *parameterInfo ,Sender DataFromSender, float (*funp_ReadDataFromSensor)(float,float)) {
   int index;
   int inner_index;
   for(index=0; index<DataFromSender.number_of_values; index++)
   {
     for(inner_index=0; inner_index<NoOfParameter; inner_index++)
     {
-      parameterInfo[inner_index].parameterdata[index] = funp_ReadDataFromSensor();
+      parameterInfo[inner_index].parameterdata[index] = funp_ReadDataFromSensor(parameterInfo[inner_index].minimumThreshold,parameterInfo[inner_index].maximumThreshold);
     }
   }
   return 1;
