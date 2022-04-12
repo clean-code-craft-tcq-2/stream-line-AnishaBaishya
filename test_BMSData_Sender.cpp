aa9e9532f47d6c4748ab0f6a1b960ad0e6d3b11e
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-12 20:02:15 +0530

    Update test_BMSData_Sender.cpp

--- test_BMSData_Sender.cpp
+++ test_BMSData_Sender.cpp
@@ -37,7 +37,7 @@ TEST_CASE("Test 1 : Prepare Data from Sensor : Valid Data") {
   float (*funp_ReadDataFromSensor)(float,float) = SimulateReadDataFromSensor;
   
   srand(time(0));
-  REQUIRE(SenderData(parameterInfo ,&DataFromSender,funp_ReadDataFromSensor) == TRUE);
+  REQUIRE(ProcessSenderData(parameterInfo ,&DataFromSender,funp_ReadDataFromSensor) == TRUE);
 }
 
 
@@ -55,5 +55,5 @@ TEST_CASE("Test 2 : Prepare Data from Sensor : Invalid Data") {
   DataFromSender1.number_of_values = 5;
   float (*funp_ReadDataFromSensor)(float,float) = SimulateReadDataFromSensorInvalid;
   
-  REQUIRE(SenderData(parameterInfo1 ,&DataFromSender1,funp_ReadDataFromSensor) == FALSE);
+  REQUIRE(ProcessSenderData(parameterInfo1 ,&DataFromSender1,funp_ReadDataFromSensor) == FALSE);
 }
