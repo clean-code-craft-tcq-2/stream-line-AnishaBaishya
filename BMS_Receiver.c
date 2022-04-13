a6ea17b3d804576b596edd78328b7fbd6ec2681a
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-04-13 10:54:53 +0530

    Create BMS_Receiver.c

---
+++ BMS_Receiver.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "BMS_Receiver.h"
+#include "BMS_Sender.h"
+
+void ReceiveDataFromConsole(BatteryParameterInfo *parameterInfo)
+{
+ 
+  int index;
+  int inner_index;
+  for(index=0;index<Number_of_values;index++)
+  {
+    for(inner_index=0; inner_index<NoOfParameter-1; inner_index++)
+    {
+	      scanf("%f ,",parameterInfo[index].parameterdata[inner_index]);
+    }
+    scanf("%f \n",parameterInfo[index].parameterdata[inner_index]);
+  }
+}
+
+void BMSReceiver(BatteryParameterInfo *parameterInfo)
+{
+  GetSensorDataFromConsole(Temperature,SOC,ChargeRate);
+  ReceiveDataFromConsole(BatteryParameterInfo *parameterInfo);
+}
