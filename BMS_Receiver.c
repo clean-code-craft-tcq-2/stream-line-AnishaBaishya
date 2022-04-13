eba19060d54beec9f77b45525d7850fda429ae7c
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-04-13 11:44:04 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -20,5 +20,5 @@ void ReceiveDataFromConsole(BatteryParameterInfo *parameterInfo)
 void BMSReceiver(BatteryParameterInfo *parameterInfo)
 {
 
-  ReceiveDataFromConsole(BatteryParameterInfo *parameterInfo);
+  ReceiveDataFromConsole(&parameterInfo);
 }
