62e51df7f145b2f3ecf95e7f5fbac1186b2ade4a
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-04-13 10:52:11 +0530

    Create BMS_Receiver.h

---
+++ BMS_Receiver.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define Number_of_values 50 //DataFromSender->number_of_values
+
+void ReceiveDataFromConsole(BatteryParameterInfo *parameterInfo);
+void BMSReceiver(BatteryParameterInfo *parameterInfo);
