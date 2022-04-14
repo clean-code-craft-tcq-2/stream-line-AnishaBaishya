b4ea96719038f9f1916ffdee102f0f4c52d00a77
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-04-14 10:13:18 +0530

    Update test_BMSData_Receiver.cpp

--- test_BMSData_Receiver.cpp
+++ test_BMSData_Receiver.cpp
@@ -2,7 +2,6 @@
 
 #include "test/catch.hpp"
 #include "BMS_Receiver.h"
-#include "BMS_Sender.h"
 
 SCENARIO("As part of receiver, read the data sent by sender")
 {
@@ -12,27 +11,11 @@ SCENARIO("As part of receiver, read the data sent by sender")
         {
             int NumberOfParameters = 2;
             int NuberOfValues = 3;
-            ReceiverBatteryParameterInfo ReceiverParameterInfo [NuberOfValues] = {0};
-            BatteryParameterInfo parameterInfo [NuberOfValues] ;
-           /* parameterInfo.parameterdata[NumberOfParameters] ={/*parameter1/{22, 23, 24},
-                                                              /*parameter2/{32, 33, 34}};*/
-                        parameterInfo ={/*parameter1*/{22, 23, 24},
-                                                              /*parameter2*/{32, 33, 34}};
+            ReceiverBatteryParameterInfo ReceiverParameterInfo [NuberOfValues];
 
             THEN("Check weather data is read from console successfully")
             {
-              Sender DataFromSender ; DataFromSender.number_of_values = 2;
-               DisplaySensorDataOnConsole(parameterInfo ,DataFromSender);
-              
                ReceiveDataFromConsole(&ReceiverParameterInfo);
-                  for(index=0;index<DataFromSender->NuberOfValues;index++)
-                  {
-                    for(inner_index=0; inner_index<NumberOfParameters-1; inner_index++)
-                    {
-                      REQUIRE(parameterInfo[inner_index].parameterdata[index]==ReceiverParameterInfo[inner_index].ReceiverParameterInfo[index]);
-                    }
-                    REQUIRE(parameterInfo[NumberOfParameters-1].parameterdata[index]==ReceiverParameterInfo[NumberOfParameters-1].ReceiverParameterInfo[index]);
-                  }
             }
         }
     }
