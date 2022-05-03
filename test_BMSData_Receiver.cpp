26308aafa819dfc0cad00a76dd8c113962d7ae99
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-05-03 23:03:07 +0530

    Update test_BMSData_Receiver.cpp

--- test_BMSData_Receiver.cpp
+++ test_BMSData_Receiver.cpp
@@ -12,10 +12,25 @@ SCENARIO("As part of receiver, read the data sent by sender")
             int NuberOfValues = 3;
             ReceiverBatteryParameterInfo ReceiverParameterInfo [NuberOfValues] = {0};
 
-            THEN("Check weather data is read from console successfully")
+            THEN("Check whether data is read from console successfully")
             {
                 REQUIRE(ReceiveDataFromConsole(ReceiverParameterInfo) == E_OK);
             }
+        }
+    }
+}
+SCENARIO("As part of receiver, read the data and process to get the statistics")
+{
+    GIVEN(" Receiver data in CSV format")
+     {
+        int NuberOfValues = 6;
+        ReceiverBatteryParameterInfo ReceiverParameterInfo [NuberOfValues] = {0};//
+        float ParameterInfo[NuberOfValues] = {5,2,6,4,3,1};
+
+        THEN("Check whether data is to find min and max values")
+        {
+            REQUIRE(ReceiveDataFromConsole(ReceiverParameterInfo) == E_OK);//
+            REQUIRE(GetMaxValueOfIncomingStream(ParameterInfo) == 6);
         }
     }
 }
