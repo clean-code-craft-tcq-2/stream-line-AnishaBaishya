c767946ecef0f2a0812ff1a7169e958ab07991e4
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-05-04 11:41:23 +0530

    Update test_BMSData_Receiver.cpp

--- test_BMSData_Receiver.cpp
+++ test_BMSData_Receiver.cpp
@@ -3,7 +3,7 @@
 #include "test/catch.hpp"
 #include "BMS_Receiver.h"
 
-SCENARIO("As part of receiver, read the data sent by sender")
+SCENARIO("Scene1: As part of receiver, read the data sent by sender")
 {
     GIVEN(" Sender data is on console")
     {
@@ -21,7 +21,7 @@ SCENARIO("As part of receiver, read the data sent by sender")
         }
     }
 }
-SCENARIO("As part of receiver, read the data and process to get the statistics")
+SCENARIO("Scene2: As part of receiver, read the data and process to get the statistics")
 {
     GIVEN(" Receiver data in CSV format")
      {
@@ -34,6 +34,27 @@ SCENARIO("As part of receiver, read the data and process to get the statistics")
             REQUIRE(GetMaxValueOfIncomingStream(ParameterData) == 6.0f);
             REQUIRE(GetMinValueofIncomingStream(ParameterData) == 1.0f);
             REQUIRE(GetSMAofIncomingStream(ParameterData, 5) == 2.2f);
+        }
+    }
+}
+
+SCENARIO("Scene3: As part of receiver, read the data and process to get the statistics and Print on console")
+{
+    GIVEN(" Receiver data in CSV format")
+     {
+        int NuberOfValues = 10; //INCOMING_STREAM_SIZE
+
+        float Temperature[NuberOfValues] = {5,2,6,4,3,1,1,2,3,4};
+        float SOC[NuberOfValues] = {5,2,6,4,3,1,1,2,3,4};
+        float ChargeRate[NuberOfValues] = {5,2,6,4,3,1,1,2,3,4};
+
+        THEN("Check whether data is to find min and max values")
+        {
+            REQUIRE(GetMaxValueOfIncomingStream(Temperature) == 6.0f);
+            REQUIRE(GetMinValueofIncomingStream(SOC) == 1.0f);
+            REQUIRE(GetSMAofIncomingStream(ChargeRate, 5) == 2.2f);
+            
+            BMSReceiver( float* Temperature, float* SOC, float* ChargeRate);
         }
     }
 }
